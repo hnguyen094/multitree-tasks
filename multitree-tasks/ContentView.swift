0564c2c7557ceaf1660d744c3921b8d22381dc4c
Hung Nguyen <hungnguyen094@gmail.com>
2024-07-03 15:05:24 +0100

    Initial Commit

---
+++ multitree-tasks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  multitree-tasks
+//
+//  Created by hung on 7/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ multitree-tasks/multitree_tasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  multitree_tasksApp.swift
+//  multitree-tasks
+//
+//  Created by hung on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct multitree_tasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
